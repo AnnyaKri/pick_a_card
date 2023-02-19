556be874f2eed551f1825b9925e499ac120ee452
anna <sobakajustdoit@gmail.com>
2023-02-19 10:34:08 +0300

    Unicode suit symbols

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,5 +1,5 @@
-#Hi
-values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-suits = %w[Diamonds Heats Clubs Spades]
+# frozen_string_literal: true
+# Hi
+values = %w[2 3 4 5 6 7 8 9 10 J Q K A]
 
-puts "#{values.sample} of #{suits.sample}"
+puts "#{values.sample} of #{%w[♦ ♠ ♥ ♣].sample}"
